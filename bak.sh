59f8e84be405be2c5f567190a202fbd3afb53d3f
ItsIsaacmcoe <isaacmcoe73@gmail.com>
2025-08-31 19:38:27 -0400

    change remote backup functionality to scale as needed with iteration add example.bak.cfg

--- bak.sh
+++ bak.sh
@@ -1,30 +1,36 @@
 #!/usr/bin/env bash
 
-#exit on command failure, undefined var and make pipeline exit codes equal to any failure
+#exit on command failure, handle undefined variables, pipeline error on any fail
 set -euo pipefail
 
 #source variable from config
 source bak.cfg
 
+#Ciphertext name
+cName=$(date +"backup-%m-%d.tar.gpg")
+
 #create local backup directory if needed
-if [ ! -d $dir ]; then
-	mkdir $dir
+if [ ! -d $lDir ]; then
+	mkdir $lDir
 fi
 
-#Try local Backup, remove empty output on failure
-if sudo tar -zcpC / -T $bList | gpg --trust-model always --yes -er $key > "$dir/$dBakName" ; then
+#Try local Backup, clean up and exit on fail
+if sudo tar -zcpC / -T $bList | gpg --trust-model always --yes -er $key > "$lDir$cName" ; then
 	res="Local:️✅ "
 else
-	rm $dir/$dBakName
+	rm $lDir/$cName
 	echo "Local:❌ "
 	exit 1
 fi
 
-#detect and copy to sd card, make modular
-if [ -d $rDriv ]; then
-	cp "$dir/$dBakName" "$rDriv/Backups/$dBakName" && res=$res"SD:️✅ "
-else
-	res=$res"SD:❌ "
-fi
+#Copy local Backup to remote directories
+for rpath in ${rDirs[@]}; do
+	if [ -d $rpath ]; then
+		cp "$lDir$cName" "$rpath$cName" && res=$res$rpath": ✅ "
+	else
+		res=$res$rpath":❌ not found "
+	fi
+done
 
+#Log output
 (date; echo $res) | sudo tee -a /var/log/backupUtility.log
